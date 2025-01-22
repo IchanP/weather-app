d5318b2c60e6c5831be9868bea91d39e8e01c9b9
Pontus Grandin <pontusgrandin6@gmail.com>
2025-01-22 18:08:21 +0100

    weatherstation models

---
+++ CurrentWeatherApp/CurrentWeatherAPI/src/model/WeatherValue.cs
@@ -0,0 +1,9 @@
+namespace CurrentWeatherAPI.src.model
+{
+    public class WeatherValue
+    {
+        public long Date { get; set; }
+        public string? Value { get; set; }
+        public string? Quality { get; set; }
+    }
+}
