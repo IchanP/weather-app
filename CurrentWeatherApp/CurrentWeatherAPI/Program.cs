b302858922f147de04c30611ddb53f58cabbee8e
IchanP <smeaurix@gmail.com>
2025-01-23 17:17:57 +0100

    connect to redis pod

--- CurrentWeatherApp/CurrentWeatherAPI/Program.cs
+++ CurrentWeatherApp/CurrentWeatherAPI/Program.cs
@@ -1,25 +1,39 @@
 using CurrentWeatherAPI.src.model;
 using CurrentWeatherAPI.src.repositories;
 using CurrentWeatherAPI.src.services;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
-string? redisString = builder.Configuration["REDIS_CONNECTION_STRING"];
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+// Ensure that all the Configuration tokens are available.
 string? httpClientName = builder.Configuration["WeatherClientName"];
-ArgumentException.ThrowIfNullOrEmpty(httpClientName);
+ArgumentException.ThrowIfNullOrEmpty(httpClientName, "HTTPClient name is missing.");
 string? weatherFetcherBaseUrl = builder.Configuration["WeatherFetcherUrl"];
-ArgumentException.ThrowIfNullOrEmpty(weatherFetcherBaseUrl);
+ArgumentException.ThrowIfNullOrEmpty(weatherFetcherBaseUrl, "Base URL to fetch from is missing.");
+string? redisString = builder.Configuration["REDIS_CONNECTION_STRING"];
+ArgumentException.ThrowIfNullOrEmpty(redisString, "Redis connection string is missing.");
 
 // Configure HttpClient with base address
 builder.Services.AddHttpClient<WeatherFetcher>(httpClientName, client =>
 {
     client.BaseAddress = new Uri(weatherFetcherBaseUrl);
 });
+
+ConfigurationOptions conf = new ConfigurationOptions
+{
+    EndPoints = { redisString },
+    AbortOnConnectFail = false
+    // TODO setup and ENABLE TLS
+};
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(conf)
+);
 
 builder.Services.AddSingleton<IWeatherFetcher<WeatherStation>, WeatherFetcher>();
 // NOTE -  Better to have it singleton and allow Redis to handle the concurrent requests
--- CurrentWeatherApp/CurrentWeatherAPI/src/repositories/WeatherRepoistory.cs
+++ CurrentWeatherApp/CurrentWeatherAPI/src/repositories/WeatherRepoistory.cs
@@ -1,9 +1,15 @@
 using CurrentWeatherAPI.src.model;
+using StackExchange.Redis;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+
 
 namespace CurrentWeatherAPI.src.repositories
 {
-    public class WeatherRepository(ILogger<WeatherRepository> logger) : IWeatherRepository<WeatherStation>
+    public class WeatherRepository(ILogger<WeatherRepository> logger, IConnectionMultiplexer redis) : IWeatherRepository<WeatherStation>
     {
+        private readonly IDatabase db = redis.GetDatabase();
+
         public async Task<List<WeatherStation>> GetWeatherData()
         {
             throw new NotImplementedException();
