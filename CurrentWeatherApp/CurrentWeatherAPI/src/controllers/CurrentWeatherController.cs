b6d66e74bcd352ba5d5c8fdcdbddb53fe2683e12
IchanP <smeaurix@gmail.com>
2025-01-30 23:40:04 +0100

    return object from route

--- CurrentWeatherApp/CurrentWeatherAPI/src/controllers/CurrentWeatherController.cs
+++ CurrentWeatherApp/CurrentWeatherAPI/src/controllers/CurrentWeatherController.cs
@@ -1,28 +1,35 @@
 
+using System.Net;
 using CurrentWeatherAPI.src.model.WeatherData;
 using CurrentWeatherAPI.src.services;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace CurrentWeatherAPI.src.controllers
 {
 
-    [Route("api/weather/current")]
+    [Route($"api{RouteName}")]
     [ApiController]
-    public class CurrentWeatherController(IWeatherApiService<WeatherData> service) : ControllerBase
+    public class CurrentWeatherController(Logger<CurrentWeatherController> logger, IWeatherApiService<WeatherData> service) : ControllerBase
     {
+        public const string RouteName = "/weather/current";
 
-        /* [HttpGet]
+        [HttpGet]
         public async Task<ActionResult<WeatherData>> GetWeatherData()
         {
             try
             {
-                // TODO fetch data and return it...
+                WeatherData returnData = await service.GetCurrentWeatherAsync();
+                logger.LogInformation($"{RouteName}: Successfully retrieved weather data. Returning 200.");
+                return Ok(returnData);
             }
             catch (Exception e)
             {
-                // TODO - Setup proper exceptions with error codes.
+                logger.LogError(e, $"{RouteName}: Failed to retrieve weather data. Returning a 500.");
+                // NOTE - Will simply return a 500 as the requester can't really be at fault... Unless we setup auth in the future
+                return StatusCode(StatusCodes.Status500InternalServerError, "Something went wrong while fetching the weather data.");
             }
-        } */
+        }
 
     }
 }
