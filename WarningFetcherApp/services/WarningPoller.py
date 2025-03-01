21309fe96e1709e9f351cd8676c7f6fee54a075c
IchanP <smeaurix@gmail.com>
2025-03-01 17:33:55 +0100

    add polling/manager structure

---
+++ WarningFetcherApp/services/WarningPoller.py
@@ -0,0 +1,14 @@
+from CommunicatorAb import CommunicatorAb
+
+class WarningPoller:
+    
+    communicator: CommunicatorAb
+    
+    def __init__(self):
+        pass
+    
+    def add_communicator(self, communicator: CommunicatorAb):
+        self.communicator = communicator
+
+
+poller = WarningPoller()
