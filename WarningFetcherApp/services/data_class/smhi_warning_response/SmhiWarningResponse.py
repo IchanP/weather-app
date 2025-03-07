d2003b01ce82c524411892e9c751303d65c476db
Pontus <smeaurix@gmail.com>
2025-03-07 22:33:50 +0100

    add optional properties to warningarea

--- WarningFetcherApp/services/data_class/smhi_warning_response/SmhiWarningResponse.py
+++ WarningFetcherApp/services/data_class/smhi_warning_response/SmhiWarningResponse.py
@@ -2,12 +2,14 @@ from pydantic import BaseModel
 from .Event import Event    
 from .AreaName import AreaName
 from .Description import Description
-
+from .WarningArea import WarningArea
 class SmhiWarningResponse(BaseModel):
     id: int
     event: Event
     areaName: AreaName
     descriptions: list[Description]
+    warningAreas: list[WarningArea]
+    
     class Config:
         extra = 'ignore'
 
