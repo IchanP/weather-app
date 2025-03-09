6216bd5698e7c808886b5875f88f5906d2a892d4
Pontus <smeaurix@gmail.com>
2025-03-09 14:11:00 +0100

    fetching error handling tests

--- WarningFetcherApp/services/SMHIWarningPoller.py
+++ WarningFetcherApp/services/SMHIWarningPoller.py
@@ -6,13 +6,18 @@ import json
 # Polls SMHI warning API
 class SMHIWarningPoller(WeatherPoller):
     
-    def fetch_and_parse_weather_data(self) -> dict:
-        # TODO - replace with real endpoint
-        url="https://opendata-download-warnings.smhi.se/ibww/test/test_2.json"
-        response = get(url)
-        data = response.text
-        sanitized_json = self._sanitize_smhi_data(data)
-        return sanitized_json
+    def fetch_and_parse_weather_data(self, url: str) -> dict:
+        try:
+            response = get(url)
+            response.raise_for_status()
+            data = response.text
+            sanitized_json = self._sanitize_smhi_data(data)
+            return sanitized_json
+        except:
+            if 400 <= response.status_code < 500:
+              raise ValueError(f"Client Error: {response.status_code} - {response.reason}")
+            else:
+                raise
         
     def _sanitize_smhi_data(self, data: str) -> list[SmhiWarningResponse]:
         try: 
