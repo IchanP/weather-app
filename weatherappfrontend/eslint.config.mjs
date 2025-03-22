da358fdd9acb9c6818ecd5651886db4d5dbd1c66
Pontus <smeaurix@gmail.com>
2025-03-22 11:11:34 +0100

    merge frontend/eslint into frontend/main

--- weatherappfrontend/eslint.config.mjs
+++ weatherappfrontend/eslint.config.mjs
@@ -1,16 +1,32 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@next/next/recommended",
+    "plugin:prettier/recommended",
+  ),
+  ...compat.config({
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "linebreak-style": "off",
+    },
+    ignorePatterns: ["*d.ts", "node_modules/", "dist/", "build/"],
+  }),
 ];
 
 export default eslintConfig;
